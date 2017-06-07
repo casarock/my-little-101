c2392625d98ccf029c5f645a8180a3b14dae75c8
Carsten Sandtner <mail@casa-rock.de>
2017-06-07 06:01:34 +0200

    Initial Commit

---
+++ my little 101/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my little 101
+//
+//  Created by Carsten Sandtner on 07.06.17.
+//  Copyright © 2017 Appsbude. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
